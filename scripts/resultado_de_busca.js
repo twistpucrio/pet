ee6ada437f6066b24ef02c568c71f6e2a2e410f3
ju-s11 <julia.g.s.jso@gmail.com>
2024-11-13 17:40:37 -0300

    criando eventos (incompleto)

--- scripts/resultado_de_busca.js
+++ scripts/resultado_de_busca.js
@@ -2,7 +2,7 @@ window.addEventListener("load", async function () {
     let botao_barra_pesquisa = document.querySelector('#botao-barra-pesquisa');
     let texto_barra_pesquisa = document.querySelector('#texto');
 
-
+    //Barra de pesquisa
     botao_barra_pesquisa.addEventListener("click", function() {
         //Aqui deve-se chamar as funções de busca de elementos na barra de pesquisa.
         
@@ -13,5 +13,31 @@ window.addEventListener("load", async function () {
             //Aqui deve-se chamar as funções de busca de elementos na barra de pesquisa.
         }
         
+    });
+
+    //Botões de Tag
+    let checkbox_doacao = document.querySelector('#doacao');
+    let checkbox_castracao = document.querySelector('#castracao');
+    let checkbox_vacinacao = document.querySelector('#vacinacao');
+    let checkbox_lazer = document.querySelector('#lazer');
+    let checkbox_saude = document.querySelector('#saude');
+
+    checkboxes.forEach(function(checkbox) {
+        checkbox.addEventListener('change', function() {
+            if (this.checked) {
+                
+            }
+        });
+    });
+
+    let radio_eventos = document.querySelector('#eventos');
+    let radio_educacionais = document.querySelector('#educacionais');
+
+    radios.forEach(function(radio) {
+        radio.addEventListener('change', function() {
+            if (this.checked) {
+                //
+            }
+        });
     });
 });
