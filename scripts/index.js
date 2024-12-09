53567f316d1c0b98e1ec2b911bca76c75a61a206
Danielle Guimarães <157411604+DanielleGuimaraesOliveira@users.noreply.github.com>
2024-12-09 11:27:33 -0300

    ajuste na quebra do codigo

--- scripts/index.js
+++ scripts/index.js
@@ -1,59 +1,33 @@
 let currentIndex = 0;
 
 function moveSlide(direction) {
-    const items = document.querySelectorAll('.carousel-item');
-    const totalItems = items.length;
+    const slides = document.querySelectorAll('.carousel-item');
+    slides[currentIndex].classList.remove('active');
 
-    items[currentIndex].classList.remove('active');
+    currentIndex = (currentIndex + direction + slides.length) % slides.length;
+    console.log("Current Index: ", currentIndex); // Depuração
 
-    currentIndex = (currentIndex + direction + totalItems) % totalItems;
-
-    items[currentIndex].classList.add('active');
+    slides[currentIndex].classList.add('active');
 }
 
-
-function limpar_barra_pesquisa() {
-    let barra_pesquisa = document.getElementById("searchInput");
-    barra_pesquisa.value = "";
-  }
-
-window.addEventListener("load", function(){ 
+window.addEventListener("load", function() {
     let campo_texto = document.getElementById("searchInput");
     let btn_barra_pesquisa = document.getElementById("botao-barra-pesquisa");
-    
-    btn_barra_pesquisa.addEventListener('click', function(event){
+
+    btn_barra_pesquisa.addEventListener('click', function(event) {
         let searchTerm = campo_texto.value;
         window.location.href = `HTML/resultado-busca.html?search=${encodeURIComponent(searchTerm)}`;
     });
 
-    campo_texto.addEventListener('keydown', function(event){
-        if(event.key === 'Enter'){
+    campo_texto.addEventListener('keydown', function(event) {
+        if (event.key === 'Enter') {
             let searchTerm = campo_texto.value;
             window.location.href = `../views/resultado_busca.html?search=${encodeURIComponent(searchTerm)}`;
         }
     });
 
     let botao_limpar = document.getElementById("botao-limpar-pesquisa");
-    botao_limpar.addEventListener("click",  function() {
-      limpar_barra_pesquisa();
-    });
-
-    const items = document.querySelectorAll(".carousel-item img");
-    let maxHeight = 0;
-
-    items.forEach((img) => {
-        img.style.height = "auto"; 
-        if (img.offsetHeight > maxHeight) {
-            maxHeight = img.offsetHeight;
-        }
-    });
-
-    const carousel = document.querySelector(".carousel");
-    carousel.style.height = `${maxHeight}px`;
-
-   
-    items.forEach((img) => {
-        img.style.height = `${maxHeight}px`;
-        img.style.objectFit = "cover"; 
+    botao_limpar.addEventListener("click", function() {
+        limpar_barra_pesquisa();
     });
 });
